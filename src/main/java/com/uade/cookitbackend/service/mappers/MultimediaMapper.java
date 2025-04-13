ffc0cb19b871b9695335dede67c05f8892d7dd33
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2025-04-13 11:16:56 -0300

    fix: idpaso fixed on multimedia mapper

--- src/main/java/com/uade/cookitbackend/service/mappers/MultimediaMapper.java
+++ src/main/java/com/uade/cookitbackend/service/mappers/MultimediaMapper.java
@@ -3,6 +3,7 @@ package com.uade.cookitbackend.service.mappers;
 import com.uade.cookitbackend.dto.MultimediaDTO;
 import com.uade.cookitbackend.entity.Multimedia;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,6 +11,7 @@ public interface MultimediaMapper {
 
     MultimediaMapper INSTANCE = Mappers.getMapper(MultimediaMapper.class);
 
+    @Mapping(target = "idPaso", source = "paso.idPaso")
     MultimediaDTO toDto(Multimedia multimedia);
     Multimedia toEntity(MultimediaDTO multimediaDTO);
 }
