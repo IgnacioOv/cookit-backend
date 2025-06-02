b0c1a0b1ef417029e4425f4df8cbe1863eafe32b
serleo <serleo96.98@gmail.com>
2025-06-02 02:22:09 -0300

    verifymail

---
+++ src/main/java/com/uade/cookitbackend/dto/VerifyMailConfirmDTO.java
@@ -0,0 +1,20 @@
+package com.uade.cookitbackend.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class VerifyMailConfirmDTO {
+    @NotBlank
+    @Email
+    @Schema(example = "user@example.com")
+    private String mail;
+
+    @NotBlank
+    @Schema(example = "123456")
+    private String code;
+
+}
+
