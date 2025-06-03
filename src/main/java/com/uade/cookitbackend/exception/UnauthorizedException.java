752226a5d4614ee4069cadc3ddaf53709fdb0aba
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2025-06-03 17:37:06 -0300

    agregado manejo de excepciones

--- src/main/java/com/uade/cookitbackend/exception/UnauthorizedException.java
+++ src/main/java/com/uade/cookitbackend/exception/UnauthorizedException.java
@@ -1,4 +1,20 @@
+// com/uade/cookitbackend/exception/UnauthorizedException.java
+
 package com.uade.cookitbackend.exception;
 
-public class UnauthorizedException {
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * Se lanza para indicar credenciales inválidas o token faltante/erróneo.
+ */
+@Getter
+public class UnauthorizedException extends RuntimeException {
+    private final ErrorCode errorCode;
+    private final HttpStatus httpStatus = HttpStatus.UNAUTHORIZED;
+
+    public UnauthorizedException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
 }
